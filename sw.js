9bf1d10da6730234789f7c64dbb7c0e25ebc3173
Temachado <machado@temachado.com.br>
2026-06-29 07:39:12 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'combustivel-1783420000';
+const CACHE = 'combustivel-1783460000';
 const FILES = ['./', './index.html', './manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
