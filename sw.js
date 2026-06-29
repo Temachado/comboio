e87ce9d5be817f343cd247bf7bcaa047150b6d49
Temachado <machado@temachado.com.br>
2026-06-29 07:26:54 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,19 +7,14 @@ self.addEventListener('message', e => {
   if(e.data && e.data.type === 'SKIP_WAITING') self.skipWaiting();
 });
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
-  );
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
   return self.clients.claim();
 });
 self.addEventListener('fetch', e => {
   if(e.request.method !== 'GET') return;
   e.respondWith(
     fetch(e.request).then(res => {
-      if(res && res.status === 200){
-        var clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-      }
+      if(res && res.status === 200){var clone=res.clone();caches.open(CACHE).then(c=>c.put(e.request,clone));}
       return res;
     }).catch(() => caches.match(e.request))
   );
